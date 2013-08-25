7c333c8405fa42a13f0af31a559554ff4c739b98
John Schimmel <john.schimmel@gmail.com>
2013-08-24 20:29:09 -0400

    separated auth and admin

--- run_server.py
+++ run_server.py
@@ -1,7 +1,9 @@
 from app import app
 from admin import admin_pages
+from auth import auth_flask_login
 
 app.register_blueprint(admin_pages, url_prefix='/admin')
+app.register_blueprint(auth_flask_login)
 
 if __name__ == "__main__":
 	app.run()
